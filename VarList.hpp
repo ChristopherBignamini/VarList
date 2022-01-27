19d7c8b2c91e029e4cc84a11202fb834740fae11
Christopher Bignamini <christopher.bignamini@gmail.com>
2022-01-27 11:26:24 +0100

    test_var_list file renamed (missing .cpp added)

--- VarList.hpp
+++ VarList.hpp
@@ -6,28 +6,47 @@ class VarList {
 public:
 
     // TODO: missing methods
-    // finalize               
-    // append                 
-    // search                 
-    // getID                  
-    // getName                
-    // getFirstVariable       
+    // finalize
+    // append
+    // search
+    // getID
+    // getName
+    // getFirstVariable
     // getNextVariable
 
-    // TODO: add check (with traits) on ContainerType, with original var list requirements 
+    // TODO: add check (with traits) on ContainerType, with original var list requirements
 
     // TODO: the varlist example int icon-kernels uses an arrai of var list, define a container for VarList
-    
-    // TODO: define copy/move ctors behaviour
 
-    VarList(std::string i_list_name): m_list_id(getGlobalId()), m_list_name(i_list_name)
+
+    VarList(std::string i_list_name)
+        :m_list_id(getGlobalId())
+        ,m_list_name(i_list_name)
     {
+    }
+
+    // TODO: define copy/move ctors behaviour
+    explicit VarList(const VarList& i_var_list)
+        :m_list(i_var_list.m_list)
+        ,m_list_id(getGlobalId())
+        ,m_list_name(i_var_list.m_list_name)
+    {
+    }
+
+    VarList& operator=(const VarList& i_var_list)
+    {
+        m_list = i_var_list.m_list;
+        m_list_id = getGlobalId();
+        m_list_name = i_var_list.m_list_name;
+
+        return *this;
     }
 
     const unsigned int getListId(void)
     {
         return m_list_id;
     }
+
 
 private:
 
