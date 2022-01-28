9341112e076fe73afdd8d7f91d71af1cbfb04ef2
Christopher Bignamini <christopher.bignamini@gmail.com>
2022-01-28 17:39:10 +0100

    new method usage example added

--- test_var_list.cpp
+++ test_var_list.cpp
@@ -17,4 +17,23 @@ int main() {
     VarList<char, double> c_d_var_list_2(c_d_var_list_1);
     std::cout<<c_d_var_list_2.getId()<<std::endl;
 
+    c_d_var_list_2.append(std::make_pair('c',23.23));
+    c_d_var_list_2.append(std::make_pair('d',46.46));
+
+    auto const * var = c_d_var_list_2.search('c');
+    std::cout<<*var<<std::endl;
+
+    auto first_variable = c_d_var_list_2.getFirstVariable();
+    std::cout<<first_variable.first<<" "<<*(first_variable.second)<<std::endl;
+
+    auto previous_variable = first_variable;
+    for(auto i=1;i<c_d_var_list_2.getListLength();i++) {
+      auto next_variable = c_d_var_list_2.getNextVariable(previous_variable);
+      std::cout<<next_variable.first<<" "<<*(next_variable.second)<<std::endl;
+      previous_variable = next_variable;
+    }
+
+    c_d_var_list_2.finalize();
+    std::cout<<c_d_var_list_2.getListLength()<<std::endl;
+
 }
