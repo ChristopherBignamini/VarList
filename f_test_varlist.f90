78d2c3a534ce72bd56a1ce516d64d1cec887573a
Christopher Bignamini <christopher.bignamini@gmail.com>
2022-02-01 18:13:56 +0100

    Second f_varlist instance added to check basic ID functionality

--- f_test_varlist.f90
+++ f_test_varlist.f90
@@ -1,12 +1,16 @@
 program test_varlist
     use libvarlist
     implicit none
-    type(varlist) :: f_varlist
+    type(varlist) :: f_varlist1, f_varlist2
 
-    f_varlist = varlist("test varlist")
-    write(*,*) f_varlist%getId()
+    f_varlist1 = varlist("test varlist 1")
+    write(*,*) f_varlist1%getId()
+
+    f_varlist2 = varlist("test varlist 2")
+    write(*,*) f_varlist2%getId()
 
 #ifdef __GNUC__
-    call f_varlist%delete
+    call f_varlist1%delete
+    call f_varlist2%delete
 #endif
 end program
