9dcdcbdbe0fbf18e65ff3e8ac3ec9296e230f1b2
Christopher Bignamini <christopher.bignamini@gmail.com>
2022-02-07 18:31:48 +0100

    Element search example added

--- f_test_varlist.f90
+++ f_test_varlist.f90
@@ -2,6 +2,7 @@ program test_varlist
     use libvarlist
     implicit none
     type(varlist) :: f_varlist1, f_varlist2
+    real*8, pointer :: val_ptr
 
     f_varlist1 = varlist("test varlist1")
     print*, f_varlist1%getId()
@@ -11,8 +12,9 @@ program test_varlist
     print*, f_varlist2%getId()
     print*, f_varlist2%getName()
     call f_varlist2%append(1,1.1D0)
-   
-    
+    val_ptr => f_varlist2%search(1)
+    if (associated(val_ptr)) print*, val_ptr
+
     
 #ifdef __GNUC__
     call f_varlist1%delete
