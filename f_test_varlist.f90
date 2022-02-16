c959c7f67343ff0548e3cbba9a0f74e7b9e20434
Christopher Bignamini <christopher.bignamini@gmail.com>
2022-02-16 12:26:21 +0100

    Next variable method usage example added

--- f_test_varlist.f90
+++ f_test_varlist.f90
@@ -2,28 +2,52 @@ program test_varlist
     use libvarlist
     use libvarlistitem
     implicit none
-    type(varlist) :: f_varlist1, f_varlist2
-    type(varlist_item) :: f_varlist_item
+    type(varlist) :: f_varlist
+    type(varlist_item) :: f_varlist_first_item, f_varlist_next_item
     real*8, pointer :: val_ptr
+    integer :: number_of_items, item_number
 
-    f_varlist1 = varlist("test varlist1")
-    print*, f_varlist1%getId()
-    print*, f_varlist1%getName()
+    ! Create varlist
+    f_varlist = varlist("test varlist")
 
-    f_varlist2 = varlist("test varlist2")
-    print*, f_varlist2%getId()
-    print*, f_varlist2%getName()
-    call f_varlist2%append(1,1.1D0)
-    val_ptr => f_varlist2%search(1)
+    ! Print varlist data
+    print*, f_varlist%getId()
+    print*, f_varlist%getName()
+
+    ! Add elements to varlist
+    call f_varlist%append(1,1.1D0)
+    call f_varlist%append(2,2.2D0)
+    call f_varlist%append(3,3.3D0)
+
+    ! Search element and print if found
+    val_ptr => f_varlist%search(1)
+    print*, "f_varlist variable 1"
+    print*, " "
     if (associated(val_ptr)) print*, val_ptr
+    print*, " "
 
-    f_varlist_item = f_varlist2%getFirstVariable()
-    print*, f_varlist_item%getName()
-    print*, f_varlist_item%getValuePtr()
-    
+    ! Get first variable and print 
+    f_varlist_first_item = f_varlist%getFirstVariable()
+    print*, "f_varlist first variable name and value"
+    print*, f_varlist_first_item%getName()
+    print*, f_varlist_first_item%getValuePtr()
+    print*, " "
+
+    ! Loop over other varlist elements    
+    number_of_items = f_varlist%getListLength()
+    item_number = 1
+    f_varlist_next_item = f_varlist_first_item
+    do while (item_number<number_of_items)
+        f_varlist_next_item = f_varlist%getNextVariable(f_varlist_next_item)
+        print*, "f_varlist next variable name and value"
+        print*, f_varlist_next_item%getName()
+        print*, f_varlist_next_item%getValuePtr()
+        print*, " "
+        item_number = item_number + 1
+     enddo
+
 #ifdef __GNUC__
-    call f_varlist1%delete
-    call f_varlist2%delete
+    call f_varlist%delete
 #endif
 end program
 
